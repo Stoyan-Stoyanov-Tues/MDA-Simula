441a66f440df4b38f5e56efe46e3637cbbc0a333
Yoann Markoff <bubobuboster@gmail.com>
2013-04-12 02:19:52 +0300

    'trying_to_separate_menu_class'

--- lib/crashTest.rb
+++ lib/crashTest.rb
@@ -1,53 +1,33 @@
 require 'rubygems'
 require 'gosu'
+require_relative 'menu'
 
 class GameWindow < Gosu::Window
-	
 	def initialize
 		super(800, 600, false)
-		self.caption = "Update/Draw Demo"
-		
-		@menuC = 0
-		@menuPics = menu_init 
+		self.caption = "Menu Demo"
 	end
+	
+	def init_menu
+		@menu = Menu.new(self)
+	end
+
 	
 	def update
 	
 	end
 	
 	def draw
-		draw_menu
-	
+		@menu.draw_menu
 	end
 	
 	def button_down(id)
-		menu_control(id)
+		@menu.menu_control(id)
 	end
 	
-	def menu_control(id)
-		close if id == Gosu::KbEscape
-		@menuC += 1 if (id == Gosu::KbS) || (id == Gosu::KbD) 
-		@menuC -= 1 if (id == Gosu::KbW) || (id == Gosu::KbA)
-		if @menuC == 4
-			@menuC = 0
-			return
-		end
-		@menuC = 3 if @menuC == -1
-	end
-	
-	def draw_menu
-		@menuPics[@menuC].draw(0,0, 255)
-	end
-		
-	def menu_init
-		pictures = Array.new(4)
-		pictures[0] = Gosu::Image.new(self, 'media/menu/new game.png', true)
-		pictures[1] = Gosu::Image.new(self, 'media/menu/load game.png', true)
-		pictures[2] = Gosu::Image.new(self, 'media/menu/highscores.png', true)
-		pictures[3] = Gosu::Image.new(self, 'media/menu/exit.png', true)
-		return pictures
-	end
 	
 end
 
-GameWindow.new.show
+MyWindow = GameWindow.new
+MyWindow.init_menu
+MyWindow.show
