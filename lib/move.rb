153cedb75cba11188a387165b287a5373e611083
lukistar <Krasimir.Stojkov@gmail.com>
2013-04-13 22:29:47 +0300

    repair simple move

--- lib/move.rb
+++ lib/move.rb
@@ -1,10 +1,25 @@
 module Move
+	def self.extended(base)
+ 	end
+ 	
 	def left
 		@pos_x-=@move_distance
+		@rectangle_x-=@move_distance
 	end
 	
 	def right
 		@pos_x+=@move_distance
+		@rectangle_x+=@move_distance
+	end
+	
+	def up
+		@pos_y-=@move_distance
+		@rectangle_y-=@move_distance
+	end
+	
+	def down
+		@pos_y+=@move_distance
+		@rectangle_y+=@move_distance
 	end
 	
 	def squat
