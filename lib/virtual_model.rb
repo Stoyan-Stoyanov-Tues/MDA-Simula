ef4131fe342ebb42930038a9033e88f94b600c65
lukistar <Krasimir.Stojkov@gmail.com>
2013-04-14 03:01:22 +0300

    small addon

--- lib/virtual_model.rb
+++ lib/virtual_model.rb
@@ -1,11 +1,15 @@
 class VirtualModel
-	attr_accessor :center, :radius, :height, :width, :distance_centers_x , :distance_centers_y
+	attr_accessor :center, :radius, :height, :width, :distance_centers_x , :distance_centers_y, :collison
 	
-	def initialize(center, radius, width, height, distance_centers_x, distance_centers_y)
+	def initialize(center, radius, width, height, distance_centers_x, distance_centers_y, preferred_collision)
 		@center = center
 		@radius = radius
 		@width, @height = width, height
+		
+		# distance between circle's center and reactangle's center
 		@distance_centers_x = distance_centers_x 
 		@distance_centers_y = distance_centers_y
+		
+		@collision = preferred_collision
 	end
 end
