c3fec054826e64b6c61551f2f806ec4be9f5748f
lukistar <Krasimir.Stojkov@gmail.com>
2013-04-14 14:27:32 +0300

    included collision detection to sprite

--- lib/sprite.rb
+++ lib/sprite.rb
@@ -1,13 +1,15 @@
 require './mobject'
 require './move.rb'
+require './base_collision'
 
 class Sprite < Mobject
 	include Move
+	include Base_collision
 	
 	attr_accessor :move_distance
 	
-	def initialize(do_collision, solid, virtual_model, move_distance)
-		super(do_collision, solid, virtual_model)
+	def initialize(do_collision, solid, virtual_model, pos_x, pos_y, move_distance)
+		super(do_collision, solid, virtual_model, pos_x, pos_y)
 		@move_distance = move_distance
 	end
 	
