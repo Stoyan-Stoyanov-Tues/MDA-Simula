278e98e80d949c21437ab52aaae431c09d84b06d
lukistar <Krasimir.Stojkov@gmail.com>
2013-05-19 23:20:43 -0500

    test move.rb

--- test/tc_move.rb
+++ test/tc_move.rb
@@ -7,9 +7,13 @@ class TestMove < Test::Unit::TestCase
 		@model = Constructor::create_virtual_model(@image)
 		$map = Map.new(800, 600, -800, -600)
 		@sprite = Sprite.new(true, true, @model, 500, 500, Hash.new)
-		@range_attack = RangeAttack.new(true, false, @model, 499, 500, Hash.new, :right)
+		@range_attack = RangeAttack.new(true, true, @model, 499, 500, Hash.new, :right)
+		@sprite_2 = Sprite.new(true, true, @model, 100, 100, Hash.new)
 		$map.add(@sprite)
+		$map.add(@sprite_2)
 		$map.add(@range_attack)
+		$player = Player.new(true, true, @model, 700, 400, Hash.new)
+		$map.add($player)
 	end
 	
 	def test_down
@@ -50,6 +54,23 @@ class TestMove < Test::Unit::TestCase
 	end
 	
 	def test_try_move?
-		
+		sprite_2 = Sprite.new(true, true, @model, 500, 500, Hash.new)
+		$map.add(sprite_2)
+		assert(!@sprite.try_move?)
+	end
+	
+	def test_move
+		@sprite_2.move(:up)
+		assert(@sprite_2.pos_y < 100)
+		@sprite.move(:up)
+		assert(@sprite.pos_x == 500)
+	end
+	
+	def test_moving
+		unit = Unit.new(true, true, @model, 250, 250, Hash.new)
+		unit.ai()
+		unit.moving()
+		assert(unit.pos_x > 250)
+		assert(unit.pos_y > 250)
 	end
 end
